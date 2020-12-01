dde3fefb8bbd0c67caa2f0319196073e316b9f90
kodecheff <akunneemeka125@gmail.com>
2020-12-01 14:32:01 +0100

    run build

---
+++ build/precache-manifest.81fbab0682a7b84d8705a222356081c3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4ca2b6ecf262a7c0ecd654468a46bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f996eb5101274cb17b3c",
+    "url": "/static/css/2.cebec226.chunk.css"
+  },
+  {
+    "revision": "754d0b5aee7dc1a80d35",
+    "url": "/static/css/main.4d1c949a.chunk.css"
+  },
+  {
+    "revision": "f996eb5101274cb17b3c",
+    "url": "/static/js/2.bd3a9a31.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bd3a9a31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "754d0b5aee7dc1a80d35",
+    "url": "/static/js/main.40aed6d2.chunk.js"
+  },
+  {
+    "revision": "f480c7621c1aa7b13b17",
+    "url": "/static/js/runtime-main.82b70976.js"
+  },
+  {
+    "revision": "4e853c74949e58ddb433c283abe84b04",
+    "url": "/static/media/Watch1.4e853c74.jpeg"
+  },
+  {
+    "revision": "0894d7a6959aff4d6244d03b806ef50b",
+    "url": "/static/media/Watch13.0894d7a6.jpeg"
+  },
+  {
+    "revision": "c197a1c624e4884e04b8495fb78d6b07",
+    "url": "/static/media/Watch15.c197a1c6.jpeg"
+  },
+  {
+    "revision": "e1bb3f34d0b92e0aad36d469d28bb039",
+    "url": "/static/media/Watch3.e1bb3f34.jpeg"
+  },
+  {
+    "revision": "4293bc8621b8c1254f901a112133f6bf",
+    "url": "/static/media/Watch4.4293bc86.jpeg"
+  },
+  {
+    "revision": "239882fb104510572b7512650793aec3",
+    "url": "/static/media/Watch5.239882fb.jpeg"
+  },
+  {
+    "revision": "e0954d662f75dbdb5a463146486e25c5",
+    "url": "/static/media/Watch6.e0954d66.jpeg"
+  },
+  {
+    "revision": "de8ed2bcee65d6b044fb9a5217e31b49",
+    "url": "/static/media/WebSlider1.de8ed2bc.png"
+  },
+  {
+    "revision": "0ec061963c27721c2e3984efae23541b",
+    "url": "/static/media/WebSlider2.0ec06196.png"
+  },
+  {
+    "revision": "c1c7bf876ac1d5b704f5168d3cdba55b",
+    "url": "/static/media/WebSlider4.c1c7bf87.png"
+  },
+  {
+    "revision": "70ae6b74ef390dfa2d2e2aac9d297971",
+    "url": "/static/media/logo.70ae6b74.png"
+  }
+]);
